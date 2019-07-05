7764a103478d26569d09b68a654287761700f03f
Beren Kuday GÖRÜN <beren.kuday.g@gmail.com>
2019-07-05 17:52:01 +0300

    geliştirilmeye devam ediliyor

--- projeler/ofise/app.js
+++ projeler/ofise/app.js
@@ -2,13 +2,23 @@ const express = require("express"); // express paketini dahil ediyoruz
 const PORT = 3000; // sunucumuzu 3000 nolu port u dinleyecek
 const app = new express(); // express i kullanarak bir sunucu referansi olusturyoruz
 
-app.set("view engine","pug"); // varsayılan engine pug oldugunu belirttik
+/*middleware işlemleri için*/
+const admin = require("./middleware/admin.js");
+/*middleware işlemleri için*/
+/*routing işlemleri için*/
+const signIn = require("./routes/signIn.js");
+/*routing işlemleri için*/
 
+app.set("view engine","pug"); // varsayılan engine pug oldugunu belirttik
+app.use(express.static("public")); /* public klasörü altındaki bütün dosyaları statik olarak belirledik.
+Bu şekilde dosyalara heryerden ulaşılabilecek.*/
 
 //ilk end-point/rotamiz localhost:3000 e yapilacak tum get requestlerini dinleyecek
-app.get("/", (req, res) => {
- res.render("index.pug", {name:"beren", surname:"gorun"});
-})
+
+app.use("/", signIn );
+
+app.use("/admin/", admin);
+
 
 /*
 bu satir sunucumuzu baslatacak ve her sey yolunda gittiyse
